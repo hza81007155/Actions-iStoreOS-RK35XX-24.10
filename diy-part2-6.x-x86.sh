d25f33ff1f3e68601e54808d5bb8da556081742d
hza81007155 <800755@qq.com>
2025-11-20 10:17:49 +0800

    Update diy-part2-6.x-x86.sh

--- diy-part2-6.x-x86.sh
+++ diy-part2-6.x-x86.sh
@@ -23,6 +23,14 @@ CONFIG_PACKAGE_nginx-mod-luci=y
 CONFIG_PACKAGE_luci-nginx=y
 CONFIG_PACKAGE_default-settings=y" >> .config
 
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+
 
 # 追加binder内核参数
 echo "CONFIG_PSI=y
